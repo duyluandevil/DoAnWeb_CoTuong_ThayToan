5d612773f91d0f67a278e7878c46eef188db7015
phunghoang3b <nguyenhoangkhai492000@gmail.com>
2021-07-17 17:19:59 +0700

    #hien con co

---
+++ DoAnWebCoTuong/Content/js/class.js
@@ -0,0 +1,26 @@
+﻿class ViTri
+{
+    constructor(left, top)
+    {
+        this.top = top;
+        this.left = left;
+    }
+}
+
+class DiemTrenBanCo {
+    constructor(id, left, top) {
+        this.id = id;
+        this.left = left;
+        this.top = top;
+    }
+}
+
+class ConCo {
+    constructor(id, loai, hinh, vitri) {
+        this.id = id;
+        this.loai = loai;
+        this.hinh = hinh;
+        this.vitri = vitri;
+    }
+}
+
